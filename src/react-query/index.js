6696613a70c399d0db1f8a8a7b36b7c3d8a4ca87
Singularity-v <crystalwu.home@gmail.com>
2022-11-28 02:11:03 +0800

    first commit

---
+++ src/react-query/index.js
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+import { getProductById, getProducts } from "../api";
+
+export const useProducts = (url) => {
+   const { data, isLoading, isError, isSuccess } = useQuery([url], getProducts)
+   return { data, isLoading, isError, isSuccess };
+};
+
+export const useProductById = (productId) => {
+   const { data, isLoading, isError, isSuccess } = useQuery(
+      [productId], getProductById
+   );
+   return { data, isLoading, isError, isSuccess };
+}
